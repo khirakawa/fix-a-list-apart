d448cff7749a3a28908115849c01c3f462ec4497
Josh Dick <josh@joshdick.net>
2013-02-12 13:45:40 -0500

    Formatting tweaks, added @match for Google Chrome, changed @include to match it :)

--- fix-a-list-apart.user.js
+++ fix-a-list-apart.user.js
@@ -1,12 +1,13 @@
 // ==UserScript==
 // @name            Fix A List Apart
 // @description     Unclip clipped title
-// @version         1.0
-// @include         htt*://alistapart*
+// @version         1.1
+// @include         *://alistapart.com/*
+// @match           *://alistapart.com/*
 // ==/UserScript==
 
-// the following jQuery load script was taken from http://stackoverflow.com/questions/2246901/how-can-i-use-jquery-in-greasemonkey-scripts-in-google-chrome
-// a function that loads jQuery and calls a callback function when jQuery has finished loading
+// Helper function that loads jQuery and calls a callback function when it has finished loading
+// Taken from http://stackoverflow.com/questions/2246901/how-can-i-use-jquery-in-greasemonkey-scripts-in-google-chrome
 function addJQuery(callback) {
   var script = document.createElement("script");
   script.setAttribute("src", "//ajax.googleapis.com/ajax/libs/jquery/1/jquery.min.js");
@@ -18,21 +19,21 @@ function addJQuery(callback) {
   document.body.appendChild(script);
 }
 
-// the guts of this userscript
+// The guts of this userscript.
 // Note, jQ replaces jQ to avoid conflicts.
 function main() {
-	// if we are not on the homepage, remove the title
+  // If we are not on the homepage, remove the title
   if(jQ("body > .global-nav").length !== 0){
     jQ(".killer-logo").remove();
-	} else {
-		jQ(".killer-logo a").css({"margin-top": "10px"});
-		jQ("figure.wide-hero").css({"margin-top": "40px"});
-	}
+  } else {
+    jQ(".killer-logo a").css({"margin-top": "10px"});
+    jQ("figure.wide-hero").css({"margin-top": "40px"});
+  }
 
-	// fix the footer
-	jQ(".deadly-subtitle").css({height:"65px"});
-	jQ(".global-footer").css({"padding-bottom": "75px"});
+  // Fix the footer
+  jQ(".deadly-subtitle").css({height:"65px"});
+  jQ(".global-footer").css({"padding-bottom": "75px"});
 }
 
-// load jQuery and execute the main function
+// Load jQuery, then execute the main function
 addJQuery(main);
